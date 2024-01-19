d39f254383833b614dbdf8542b650e0468b06b90
theVakhovskeIsTaken <thevakhovske@petalmail.com>
2024-01-19 18:02:27 +0000

    Prepare post-update hooks for AYN Devices

--- etc/lib/holoiso-hwsupport/him_devicequirks/LokiZero.sh
+++ etc/lib/holoiso-hwsupport/him_devicequirks/LokiZero.sh
@@ -4,4 +4,4 @@
 export HOLOISO_GAMESCOPE_ADDITIONAL_OPTIONS="--force-orientation left"
 export HOLOISO_GAMESCOPE_RES="-w 1280 -h 720"
 export HOLOISO_X11_ROTATION="left"
-export HOLOISO_DEVICE_TYPE="LokiZero"
+export HOLOISO_DEVICE_TYPE="Ayn"
